f520d5539853ed375021fedb8e5b876eefb223fc
Siddhant Shukla <shuklasiddhant365@gmail.com>
2025-06-12 20:38:02 +0530

    adding 137

---
+++ 137.java
@@ -0,0 +1,14 @@
+// LeetCode Problem 136: Single Number 2...
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        int ones = 0;
+        int twos = 0;
+
+        for (int num : nums) {
+            ones = (ones ^ num) & ~twos;
+            twos = (twos ^ num) & ~ones;
+        }
+        return ones;
+    }
+}
