c8d1d28d0161c75bcb02501b33365b08956d0df0
Siddhant Shukla <shuklasiddhant365@gmail.com>
2025-06-14 21:34:37 +0530

    add

---
+++ Solution.java
@@ -0,0 +1,21 @@
+// LeetCode Problem 704: Binary Search
+
+public class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;  // Prevents integer overflow
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;  // Search in right half
+            } else {
+                right = mid - 1; // Search in left half
+            }
+        }
+
+        return -1;  // Target not found
+    }
+}
