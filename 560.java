e91f46a7a2e03efdec29e665b8dc3058ac4cad7e
Siddhant Shukla <shuklasiddhant365@gmail.com>
2025-06-21 06:23:10 +0530

    add

---
+++ 560.java
@@ -0,0 +1,22 @@
+// LeetCode Problem 560: SubArray sum equals to k...
+
+
+public class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int count = 0, sum = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1); // Initial sum 0 has 1 occurrence
+
+        for (int num : nums) {
+            sum += num;
+
+            if (map.containsKey(sum - k)) {
+                count += map.get(sum - k);
+            }
+
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+
+        return count;
+    }
+}
