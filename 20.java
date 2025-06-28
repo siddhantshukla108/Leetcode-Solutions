2118c4b78a88ae07d84497c7552f3c98586e107e
Siddhant Shukla <shuklasiddhant365@gmail.com>
2025-06-28 23:16:33 +0530

    add

---
+++ 20.java
@@ -0,0 +1,23 @@
+// LeetCode Problem 20: Valid Paranthesis...
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) return false;
+                char top = stack.pop();
+                if ((c == ')' && top != '(') ||
+                    (c == '}' && top != '{') ||
+                    (c == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
