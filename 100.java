09e662ad482dc61668f4ce87eee8b72d07395b00
Siddhant Shukla <shuklasiddhant365@gmail.com>
2025-07-08 23:36:11 +0530

    Create 100.java

---
+++ 100.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        // Both trees are empty
+        if (p == null && q == null) return true;
+
+        // One of the trees is empty
+        if (p == null || q == null) return false;
+
+        // Check current node values and recurse for left and right subtrees
+        return (p.val == q.val)
+            && isSameTree(p.left, q.left)
+            && isSameTree(p.right, q.right);
+    }
+}
